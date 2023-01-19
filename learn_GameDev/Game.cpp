bede97b89342414798b837f62f9d5c63faccdcb6
jdt <Danieltorogil@gmail.com>
2023-01-19 10:16:57 -0600

    ??

--- learn_GameDev/Game.cpp
+++ learn_GameDev/Game.cpp
@@ -4,8 +4,10 @@
 Game::Game()
 	: window(sf::VideoMode(600, 600), "GAME!!"), player()
 {
-	ptrDir = new bool[4]; 
+
+	// create the player
 	player.setRadius(40.f); 
+	player.setOrigin(player.getRadius() / 2, player.getRadius() / 2);
 	player.setPosition(100.f, 100.f); 
 	player.setFillColor(sf::Color::Cyan); 
 }
@@ -13,16 +15,23 @@ Game::Game()
 
 Game::~Game() 
 { 
-	delete[] ptrDir; 
 }
 
 
 void Game::run()
 {
+	sf::Clock clock; 
+	sf::Time timeSinceLastUpdate = sf::Time::Zero; 
 	while (window.isOpen())
 	{
 		processEvents(); 
-		update(); 
+		timeSinceLastUpdate += clock.restart(); 
+		while (timeSinceLastUpdate > TimePerFrame)
+		{
+			timeSinceLastUpdate -= TimePerFrame;
+			processEvents(); 
+			update(TimePerFrame); 
+		}
 		render(); 
 	}
 }
@@ -35,13 +44,15 @@ void Game::processEvents()
 	{
 		switch (evnt.type)
 		{
-		case sf::Event::Closed:
-			window.close(); 
 		case sf::Event::KeyPressed:
-
+			handlePlayerInput(evnt.key.code, true); 
+			break;
 		case sf::Event::KeyReleased:
-
-
+			handlePlayerInput(evnt.key.code, false); 
+			break; 
+		case sf::Event::Closed: 
+			window.close(); 
+			break; 
 		}
 	}
 }
@@ -49,13 +60,57 @@ void Game::processEvents()
 
 void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
 {
-	
+	if (isPressed)
+	{
+		p_isMoving = true;
+		switch (key)
+		{
+		case sf::Keyboard::Key::W:
+			p_dir = 0;
+			break;
+		case sf::Keyboard::Key::D:
+			p_dir = 1;
+			break;
+		case sf::Keyboard::Key::S:
+			p_dir = 2;
+			break;
+		case sf::Keyboard::Key::A:
+			p_dir = 3;
+			break;
+		}
+	}
+	else
+	{
+		p_dir = -1; 
+		p_isMoving = false; 
+	}
 }
 
 
-void Game::update()
+void Game::update(sf::Time dt)
 {
-
+	p_speed = 200.f;
+	offset = { 0.f, 0.f }; 
+	if (p_isMoving)
+	{
+		switch (p_dir)
+		{
+			case NORTH:
+				offset.y -= p_speed;
+				break;
+			case EAST:
+				offset.x += p_speed;
+				break;
+			case SOUTH: 
+				offset.y += p_speed; 
+				break;
+			case WEST:
+				offset.x -= p_speed;
+				break;
+		}
+	
+		player.move(offset * dt.asSeconds()); 
+	}
 }
 
 
--- learn_GameDev/Game.h
+++ learn_GameDev/Game.h
@@ -10,12 +10,27 @@ public:
 private:
 	void processEvents();
 	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed); 
-	void update();
+	void update(sf::Time);
 	void render();
 	
 private:
-	bool* ptrDir; 
+	// sfml screen
 	sf::RenderWindow window; 
+
+	// indicator for headeing 
+	enum directions {NORTH = 0, EAST, SOUTH, WEST};
+
+	sf::Time TimePerFrame = sf::seconds(1.f / 60.f); 
+
+	// Player variable (will be a class in the future)
 	sf::CircleShape player; 
+	// bool to determine stillness 
+	bool p_isMoving = false; 
+	// player heading
+	int p_dir;
+	// player speed
+	int p_speed; 
+	// offset to move player
+	sf::Vector2f offset;
 };
 
